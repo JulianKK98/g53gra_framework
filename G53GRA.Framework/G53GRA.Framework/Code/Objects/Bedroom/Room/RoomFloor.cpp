e56ca5cf45a367cbf227a492e0257a94b81b0bf0
Julian Kyotaro Kubelec <juliankubelec23@gmail.com>
2020-04-25 12:49:56 +0100

    making floor

--- G53GRA.Framework/G53GRA.Framework/Code/Objects/Bedroom/Room/RoomFloor.cpp
+++ G53GRA.Framework/G53GRA.Framework/Code/Objects/Bedroom/Room/RoomFloor.cpp
@@ -1,6 +1,15 @@
 #include <RoomFloor.h>
 
+RoomFloor::RoomFloor(){}
 
-RoomFloor::Display() {
+void RoomFloor::Display() 
+{
+	glTranslatef(pos[0], pos[1], pos[2]);
+	glScalef(scale[0], scale[1], scale[2]);
+	glRotatef(rotation[1], 0.f, 1.f, 0.f); //y
+	glRotatef(rotation[2], 0.f, 0.f, 1.f); //z
+	glRotatef(rotation[0], 1.f, 0.f, 0.f); //x
+
+	glBegin(GL_POLYGON);
 
 }
